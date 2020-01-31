c9d938dfd4324028ccd1c6245401bb49415b6513
cherryApp <j.cserko@gmail.com>
2020-01-31 12:17:51 +0100

    Feat: paginator has been completed.

--- src/app/page/paginator/paginator.component.ts
+++ src/app/page/paginator/paginator.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild, OnDestroy } from '@angular/core';
 import { UserService } from 'src/app/service/user.service';
 import { User } from 'src/app/model/user';
 import { MatTableDataSource } from '@angular/material/table';
 import { MatPaginator } from '@angular/material/paginator';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-paginator',
   templateUrl: './paginator.component.html',
   styleUrls: ['./paginator.component.scss']
 })
-export class PaginatorComponent implements OnInit {
+export class PaginatorComponent implements OnInit, OnDestroy {
 
   dataSource: MatTableDataSource<User> = new MatTableDataSource<User>();
   displayedColumns: string[] = [
@@ -21,6 +22,7 @@ export class PaginatorComponent implements OnInit {
     'address'
   ];
   pageSizes: number[] = [5, 10, 25, 100];
+  dataSubscription: Subscription;
 
   @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
 
@@ -30,11 +32,15 @@ export class PaginatorComponent implements OnInit {
 
   ngOnInit() {
     this.dataSource.paginator = this.paginator;
-    this.userService.get().subscribe(
+    this.dataSubscription = this.userService.get().subscribe(
       users => {
         this.dataSource.data = (users as unknown as User[]);
       }
     );
+  }
+
+  ngOnDestroy() {
+    this.dataSubscription.unsubscribe();
   }
 
 }
